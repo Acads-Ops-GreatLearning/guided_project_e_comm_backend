ab44e61abb49c03602bc016cc0cd402e5b7df618
Progy1992 <32261340+Progy1992@users.noreply.github.com>
2023-10-11 14:59:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const corsOptions = {
     allowedHeaders: 'Authorization,Content-Type',  // Specify the allowed headers
     credentials: true,              // Allow credentials (e.g., cookies)
 };
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Connecting to the database
 connectDatabase();
